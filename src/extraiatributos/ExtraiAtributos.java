daba3618a92910ea5e98419dfbb5b3165847f16e
MatheusDornelas <mdr@cin.ufpe.br>
2017-11-09 09:36:33 -0300

    Adicionando escrita em arquivo

--- src/extraiatributos/ExtraiAtributos.java
+++ src/extraiatributos/ExtraiAtributos.java
@@ -3,7 +3,12 @@ package extraiatributos;
 
 import static extraiatributos.ExtraiAtributos.CLASSE;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jpcap.JpcapCaptor;
@@ -17,10 +22,30 @@ public class ExtraiAtributos {
     static ArrayList<String> saida = new ArrayList<String>();
     static final String CLASSE = "web";
     static String url_fluxos_rotulados = "C:\\Users\\Matheus\\eclipse-workspace\\ExtrairAtributos\\Fluxos Rotulados\\web\\" + CLASSE;
+	static Path file = Paths.get("weka_input_" + CLASSE + ".arff");
 
-    public static void main(String[] args) {
+    public static void escreveCabecalho () throws IOException {
+		//Escreve o cabeçalho do arquivo de input do Weka
+		List<String> cabecalho = new ArrayList();
+
+		cabecalho.add("@ATTRIBUTE TamanhoJanela			NUMERIC");
+		cabecalho.add("@ATTRIBUTE TamanhoPayload			NUMERIC");
+		cabecalho.add("@ATTRIBUTE CompCabecalhoTCP		NUMERIC");
+		cabecalho.add("@ATTRIBUTE CompPacote		                NUMERIC");
+		cabecalho.add("@ATTRIBUTE class 					{p2p,web, ftp}");
+		cabecalho.add("");
+		cabecalho.add("");
+		cabecalho.add("");
+		
+		//Ficar atento com problemas de codificação caso o Weka dê erro.
+		Files.write(file, cabecalho, Charset.forName("UTF-8"));
+    }
     
-        for (int i = 1; i < 2; i++) {
+    public static void main(String[] args) throws IOException {
+    	
+    	escreveCabecalho();
+    	
+    	for (int i = 1; i < 2; i++) {
             try {
             	System.out.println(url_fluxos_rotulados);
                 fluxoAlvo = JpcapCaptor.openFile(url_fluxos_rotulados + "\\Fluxo (" + i + ").pcap");
--- src/extraiatributos/extrairtcp.java
+++ src/extraiatributos/extrairtcp.java
@@ -1,8 +1,15 @@
 package extraiatributos;
 
 
+import java.io.IOException;
 import java.lang.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import jpcap.JpcapCaptor;
 import jpcap.NetworkInterface;
@@ -12,93 +19,99 @@ import jpcap.packet.Packet;
 import jpcap.packet.TCPPacket;
 
 public class extrairtcp {
-    
-   static NetworkInterface[] array;
 
-    public static void extraindo(JpcapCaptor pcaptor) {
-    	
-    	//Atributos estat�sticos
-        long quant_bytes = 0;
-        long maior_bytes = 0;
-        long menor_bytes = 0;        
-        int janela = 0;
-        int payload = 0;  
-        int comp_cabecalho = 0;
-        long num_sequencia = 0;
-        long num_ack = 0;
-        int porta_origem = 0;
-        int porta_dest = 0;
-        
-        
-        //lista para receber os pacotes
-        final List<Packet> pacotes = new ArrayList<>();
-        pcaptor.loopPacket(-1, new PacketReceiver() {
-            @Override
-            public void receivePacket(Packet packet) {
-                if (packet instanceof IPPacket) {
-                    pacotes.add(packet);
-                }
-            }
-        });
+	static NetworkInterface[] array;
+	static Path file = Paths.get("weka_input_web.arff");
 
-        //percorrendo a lista de pacotes para calcular os atributos
-        int contador = 0;
-        for (Packet packet : pacotes) {
+	public static void escreveArquivo (List<String> fluxo) throws IOException {
+		Files.write(file, fluxo, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+	}
+	public static void extraindo(JpcapCaptor pcaptor) throws IOException {    	
+		
+		//Atributos estat�sticos
+		long quant_bytes = 0;
+		long maior_bytes = 0;
+		long menor_bytes = 0;        
+		int janela = 0;
+		int payload = 0;  
+		int comp_cabecalho = 0;
+		long num_sequencia = 0;
+		long num_ack = 0;
+		int porta_origem = 0;
+		int porta_dest = 0;
+		List<String> fluxos = new ArrayList<>();
+		
+		//lista para receber os pacotes
+		final List<Packet> pacotes = new ArrayList<>();
+		pcaptor.loopPacket(-1, new PacketReceiver() {
+			@Override
+			public void receivePacket(Packet packet) {
+				if (packet instanceof IPPacket) {
+					pacotes.add(packet);
+				}
+			}
+		});
 
-            if (packet instanceof TCPPacket) {
-                TCPPacket tcp = (TCPPacket) packet;
-                System.out.println(tcp);
-                
-                //Pra que serve o n�mero de bytes???
-                // n�mero de bytes
-                quant_bytes = quant_bytes + tcp.caplen;
+		//percorrendo a lista de pacotes para calcular os atributos
+		int contador = 0;
+		for (Packet packet : pacotes) {
 
-                if (contador==0){ //pega o menor valor na primeira interaÃ§Ã£o 
-                    menor_bytes = quant_bytes;
-                }
-                if (quant_bytes < menor_bytes){
-                  menor_bytes = quant_bytes; // retorna o menor valor de bytes
-                }
-                if(quant_bytes > maior_bytes){
-                 maior_bytes = quant_bytes; //retorna o maior valor
-                } 
-                
-                //obtÃ©m o Tamanho da janela
-                janela = janela + tcp.window;
+			if (packet instanceof TCPPacket) {
+				TCPPacket tcp = (TCPPacket) packet;
+				System.out.println(tcp);
 
-                //obtÃ©m o tamanho do payload
-                payload = payload + tcp.data.length;
+				//Pra que serve o n�mero de bytes???
+				// n�mero de bytes
+				quant_bytes = quant_bytes + tcp.caplen;
 
-                //obtÃ©m o comprimento do cabeÃ§alho TCP 
-                comp_cabecalho = comp_cabecalho + tcp.header.length;
+				if (contador==0){ //pega o menor valor na primeira interaÃ§Ã£o 
+					menor_bytes = quant_bytes;
+				}
+				if (quant_bytes < menor_bytes){
+					menor_bytes = quant_bytes; // retorna o menor valor de bytes
+				}
+				if(quant_bytes > maior_bytes){
+					maior_bytes = quant_bytes; //retorna o maior valor
+				} 
 
-                //obtÃ©m o nÃºmero de seqÃ¼Ãªncia de pacotes.
-                num_sequencia = num_sequencia + tcp.sequence;                                                                                               
-                
-                //obtÃ©m o nÃºmero dados recebidos 
-                num_ack = num_ack + tcp.ack_num;
-                
-                //obtÃ©m a porta origem
-                porta_origem = porta_origem + tcp.src_port;
-                
-                //obtÃ©m a porta destino
-                porta_dest = porta_dest + tcp.dst_port;
-                
-                contador++;
-            }
+				//obtÃ©m o Tamanho da janela
+				janela = janela + tcp.window;
 
-        }
+				//obtÃ©m o tamanho do payload
+				payload = payload + tcp.data.length;
 
-        long media_janela = janela / contador; // media tamanho da janela
+				//obtÃ©m o comprimento do cabeÃ§alho TCP 
+				comp_cabecalho = comp_cabecalho + tcp.header.length;
 
-        int media_payload = payload / contador; //media payload
+				//obtÃ©m o nÃºmero de seqÃ¼Ãªncia de pacotes.
+				num_sequencia = num_sequencia + tcp.sequence;                                                                                               
 
-        int media_comprimento = comp_cabecalho / contador; //media comprimento do cabeÃ§alho TCP
+				//obtÃ©m o nÃºmero dados recebidos 
+				num_ack = num_ack + tcp.ack_num;
 
-        long media_num_sequencia = num_sequencia / contador; //media do nÃºmero de seqÃ¼Ãªncia de pacotes.
-        
-        long media_num_ack = num_ack / contador; //media do nÃºmero de dados recebidos 
+				//obtÃ©m a porta origem
+				porta_origem = porta_origem + tcp.src_port;
 
-        System.out.println(maior_bytes+ "," +menor_bytes+ "," +janela+ "," +media_janela+ "," +payload+ "," +media_payload+ "," +comp_cabecalho+ "," + media_comprimento + "," +num_sequencia+ "," + media_num_sequencia+ "," +num_ack+ "," +media_num_ack+ ",p2p");
-    }
+				//obtÃ©m a porta destino
+				porta_dest = porta_dest + tcp.dst_port;
+
+				contador++;
+			}
+
+		}
+
+		long media_janela = janela / contador; // media tamanho da janela
+
+		int media_payload = payload / contador; //media payload
+
+		int media_comprimento = comp_cabecalho / contador; //media comprimento do cabeÃ§alho TCP
+
+		long media_num_sequencia = num_sequencia / contador; //media do nÃºmero de seqÃ¼Ãªncia de pacotes.
+
+		long media_num_ack = num_ack / contador; //media do nÃºmero de dados recebidos 
+		
+		fluxos.add(maior_bytes+ "," +menor_bytes+ "," +janela+ "," +media_janela+ "," +payload+ "," +media_payload+ "," +comp_cabecalho+ "," + media_comprimento + "," +num_sequencia+ "," + media_num_sequencia+ "," +num_ack+ "," +media_num_ack+ ",p2p");
+		escreveArquivo(fluxos);
+		System.out.println(maior_bytes+ "," +menor_bytes+ "," +janela+ "," +media_janela+ "," +payload+ "," +media_payload+ "," +comp_cabecalho+ "," + media_comprimento + "," +num_sequencia+ "," + media_num_sequencia+ "," +num_ack+ "," +media_num_ack+ ",p2p");
+	}
 }
